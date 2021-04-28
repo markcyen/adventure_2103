c363a1b74c8f766c88087e28b4e442080ba4c8d6
MarkY <markcyen@yahoo.com>
2021-04-28 10:49:39 -0600

    Condense test for favorite snack method

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -90,14 +90,6 @@ RSpec.describe Hiker do
 
   context '#favorite_snack' do
     it 'finds hikers favorite snack' do
-      trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
-      trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
-      trail3 = Trail.new({name: 'Chimney Rock Loop', length: '3.6 miles', level: :strenuous})
-      trail4 = Trail.new({name: "Queen's/Navajo Loop", length: '2.9 miles', level: :moderate})
-      trail5 = Trail.new({name: 'Rim Trail', length: '11 miles', level: :easy})
-      trail6 = Trail.new({name: 'Tower Bridge', length: '3 miles', level: :moderate})
-      park1 = Park.new('Capitol Reef')
-      park2 = Park.new('Bryce Canyon')
       hiker = Hiker.new('Dora', :moderate)
       hiker.pack('water', 2)
       hiker.pack('trail mix', 1)
