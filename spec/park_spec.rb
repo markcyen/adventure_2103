903deb023a6088569d38756be2bb6a3a19c07be3
MarkY <markcyen@yahoo.com>
2021-04-28 09:14:24 -0600

    Create test to add trail method

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -18,5 +18,16 @@ RSpec.describe Park do
       expect(park1.name).to eq('Capitol Reef')
       expect(park1.trails).to eq([])
     end
+
+    it 'has trails added' do
+      park1 = Park.new('Capitol Reef')
+      trail1 = Trail.new({name: 'Grand Wash', length: '2.2 miles', level: :easy})
+      trail2 = Trail.new({name: 'Cohab Canyon', length: '1.7 miles', level: :moderate})
+      park1.add_trail(trail1)
+      park1.add_trail(trail2)
+
+      expected = [trail1, trail2]
+      expect(park1.trails).to eq(expected)
+    end
   end
 end
