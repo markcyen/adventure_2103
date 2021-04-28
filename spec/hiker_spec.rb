7764739bcd34f5f187bb5d83581b3344c2dd48d9
MarkY <markcyen@yahoo.com>
2021-04-28 09:18:15 -0600

    Create initialize teset

--- spec/hiker_spec.rb
+++ spec/hiker_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Hiker do
       expect(hiker).to be_instance_of(Hiker)
     end
   end
+
+  context '#initialize' do
+    it 'has attributes' do
+      hiker = Hiker.new('Dora', :moderate)
+
+      expect(hiker.name).to eq('Dora')
+      expect(hiker.experience_level).to eq(:moderate)
+      expect(hiker.snacks).to eq({})
+    end
+  end
 end
