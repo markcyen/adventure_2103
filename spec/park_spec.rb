e8193783a006b4dffabb3196872064920b88f9bb
MarkY <markcyen@yahoo.com>
2021-04-28 09:10:51 -0600

    Create initialize test

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe Park do
       expect(park1).to be_instance_of(Park)
     end
   end
+
+  context '#initialize' do
+    it 'has attributes' do
+      park1 = Park.new('Capitol Reef')
+
+      expect(park1.name).to eq('Capitol Reef')
+      expect(park1.trails).to eq([])
+    end
+  end
 end
